faa34d141612702e91f1f25908055f451904c6a9
vuongshou <vuongshou@gmail.com>
2021-12-02 18:38:41 +0700

    update2

---
+++ Project_UITGreen_admin/Controllers/ProductController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_UITGreen_admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_UITGreen_admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index(int pg = 1)
+        {
+            List<Product> listPro = new List<Product>();
+            listPro = Product.SelectPro();
+            List<Image> listImg = new List<Image>();
+            foreach (var item in listPro)
+            {
+                listImg.Add(Image.SelectOne(item.id_img));
+            }
+            this.ViewBag.listImg = listImg;
+            List<Category> listCat = new List<Category>();
+            foreach (var item1 in listPro)
+            {
+                listCat.Add(Category.FindCatByID(item1.id_cat));
+            }
+            this.ViewBag.listCat = listCat;
+
+            const int pageSize = 5;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+            int recsCount = listPro.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = listPro.Skip(recSkip).Take(pager.pageSize).ToList();
+            this.ViewBag.Pager = pager;
+            return View(data);
+        }
+        [HttpGet]
+        [Route("searchpro")]
+        public IActionResult SearchPro(int pg = 1)
+        {
+            string search = Request.Query["search"].ToString();
+            List<Product> listPro = new List<Product>();
+            listPro = Product.SearchPro(search);
+            List<Image> listImg = new List<Image>();
+            foreach (var item in listPro)
+            {
+                listImg.Add(Image.SelectOne(item.id_img));
+            }
+            this.ViewBag.listImg = listImg;
+            List<Category> listCat = new List<Category>();
+            foreach (var item1 in listPro)
+            {
+                listCat.Add(Category.FindCatByID(item1.id_cat));
+            }
+            this.ViewBag.listCat = listCat;
+
+            const int pageSize = 5;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+            int recsCount = listPro.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = listPro.Skip(recSkip).Take(pager.pageSize).ToList();
+            this.ViewBag.Pager = pager;
+            this.ViewBag.search = search;
+            return View("Index", data);
+        }
+        public IActionResult Insert()
+        {
+            List<Category> listCat = new List<Category>();
+            listCat = Category.FindCatChild();
+
+            return View(listCat);
+        }
+        public IActionResult Update(int id)
+        {
+            List<Category> listCat = new List<Category>();
+            listCat = Category.FindCatChild();
+            Product pro = Product.FindProByID(id);
+            this.ViewBag.pro = pro;
+            Image img = Image.SelectOne(pro.id_img);
+            this.ViewBag.img = img;
+            return View(listCat);
+        }
+        public IActionResult UpdatePro(Product pro, string link)
+        {
+            Product.UpdatePro(pro, link);
+            return RedirectToAction("Index");
+        }
+        public IActionResult InsertPro(Image img, Product pro)
+        {
+            Image.InsertImg(img);
+            Image image = Image.SelectNew();
+            Product.InsertPro(pro, image.id_img);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Product pro = Product.FindProByID(id);
+            int id_img = pro.id_img;
+            Product.DeletePro(id);
+            Image.DeleteImg(id_img);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Project_UITGreen_admin/Models/Image.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_UITGreen_admin.Models
+{
+    public class Image
+    {
+        public int id_img { set; get; }
+        public string link { set; get; }
+
+        // xử lý
+        public static List<Image> SelectImg()
+        {
+            List<Image> listImg = new List<Image>();
+            using (var context = new DataContext())
+            {
+                var img = context.Image.ToList();
+                foreach (var image in img)
+                {
+                    listImg.Add(new Image()
+                    {
+                        id_img = image.id_img,
+                        link = image.link
+                    });
+                }
+            }
+            return listImg;
+        }
+        public static void InsertImg(Image img)
+        {
+            using (var context = new DataContext())
+            {
+                context.Image.Add(new Image
+                {
+                    id_img = img.id_img,
+                    link = img.link
+                });
+                context.SaveChanges();
+            }
+        }
+        public static void UpdateImg(Image img1)
+        {
+            using (var context = new DataContext())
+            {
+                var image = context.Image;
+                Image img = (from p in image
+                             where (p.id_img == img1.id_img)
+                             select p).FirstOrDefault();
+                if (img != null)
+                {
+                    img.link = img1.link;
+                    context.SaveChanges();
+                }
+            }
+        }
+        public static Image SelectOne(int id)
+        {
+            using (var context = new DataContext())
+            {
+                var image = context.Image;
+                Image img = (from p in image
+                             where (p.id_img == id)
+                             select p).FirstOrDefault();
+                return img;
+            }
+        }
+        public static Image SelectNew()
+        {
+            using (var context = new DataContext())
+            {
+                Image image = context.Image.ToList().Last();
+                return image;
+            }
+        }
+        public static void DeleteImg(int id)
+        {
+            using (var context = new DataContext())
+            {
+                var img = context.Image;
+                var image = (from p in img
+                             where (p.id_img == id)
+                             select p).FirstOrDefault();
+                if (image != null)
+                {
+                    context.Remove(image);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
