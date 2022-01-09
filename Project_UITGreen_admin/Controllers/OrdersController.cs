6701a15882cf4be7161939ce247962d83c5d394c
lucasph94 <phantandat97@gmail.com>
2022-01-09 09:40:47 +0700

    update export invoice

--- Project_UITGreen_admin/Controllers/OrdersController.cs
+++ Project_UITGreen_admin/Controllers/OrdersController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Project_UITGreen_admin.Models;
-using Aspose.Pdf;
+//using Aspose.Pdf;
 using Aspose.Pdf.Text;
 using System.Data;
 using System.IO;
 using System.ComponentModel;
+using Syncfusion.XlsIO;
+using Syncfusion.Drawing;
 
 namespace Project_UITGreen_admin.Controllers
 {
@@ -149,7 +151,8 @@ namespace Project_UITGreen_admin.Controllers
 
             return View();
         }
-        public IActionResult ExportSaleOrd(int id)
+
+        /*public IActionResult ExportSaleOrd(int id)
         {
             Orders ord1 = Orders.SelectOrdByID(id);
             Customer cus = Customer.SelectByID(ord1.id_customer);
@@ -225,6 +228,186 @@ namespace Project_UITGreen_admin.Controllers
                 {
                     FileDownloadName = $"Hóa đơn {cus.name_cus}.pdf"
                 };
+            }
+        }
+        */
+        public IActionResult CreateDocument()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Tạo workbook
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Logo
+                FileStream imageStream = new FileStream("wwwroot/image/logohoadon.png", FileMode.Open, FileAccess.Read);
+                IPictureShape shape = worksheet.Pictures.AddPicture(1, 1, imageStream);
+
+                //Xóa gridline
+                worksheet.IsGridLinesVisible = false;
+
+                //Thông tin giới thiệu
+                worksheet.Range["A3"].Text = "Trường Đại học Công nghệ Thông tin - ĐHQG TP.HCM";
+                worksheet.Range["A4"].Text = "Khu phố 6, phường Linh Trung, TP.Thủ Đức, TP.HCM";
+                worksheet.Range["A5"].Text = "Phone: 0123456789";
+
+                //In đậm text
+                worksheet.Range["A3:A5"].CellStyle.Font.Bold = true;
+
+                //Merge cells
+                worksheet.Range["D1:E1"].Merge();
+
+                //Enter text to the cell D1 and apply formatting.
+                worksheet.Range["D1"].Text = "HÓA ĐƠN";
+                worksheet.Range["D1"].CellStyle.Font.Bold = true;
+                worksheet.Range["D1"].CellStyle.Font.RGBColor = Color.FromArgb(0, 137, 71);
+                worksheet.Range["D1"].CellStyle.Font.Size = 35;
+                worksheet.Range["D1"].CellStyle.Font.FontName = "Arial";
+
+                //Apply alignment in the cell D1
+                worksheet.Range["D1"].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignRight;
+                worksheet.Range["D1"].CellStyle.VerticalAlignment = ExcelVAlign.VAlignTop;
+
+                //Thông tin đơn hàng
+                worksheet.Range["D5"].Text = "MÃ ĐƠN HÀNG";
+                worksheet.Range["E5"].Text = "NGÀY LẬP";
+                worksheet.Range["D6"].Number = 1; 
+                worksheet.Range["E6"].Value = "1/1/2022";
+                worksheet.Range["D7"].Text = "MÃ KHÁCH HÀNG";
+                worksheet.Range["E7"].Text = "THANH TOÁN";
+                worksheet.Range["D8"].Number = 2;
+                worksheet.Range["E8"].Text = "Tiền mặt";
+
+                //Format
+                worksheet.Range["D5:E5"].CellStyle.Color = Color.FromArgb(0, 137, 71);
+                worksheet.Range["D7:E7"].CellStyle.Color = Color.FromArgb(0, 137, 71);
+
+                worksheet.Range["D5:E5"].CellStyle.Font.Color = ExcelKnownColors.White;
+                worksheet.Range["D7:E7"].CellStyle.Font.Color = ExcelKnownColors.White;
+
+                worksheet.Range["D5:E8"].CellStyle.Font.Bold = true;
+
+                worksheet.Range["D5:E8"].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignCenter;
+                worksheet.Range["D5:E5"].CellStyle.VerticalAlignment = ExcelVAlign.VAlignCenter;
+                worksheet.Range["D7:E7"].CellStyle.VerticalAlignment = ExcelVAlign.VAlignCenter;
+                worksheet.Range["D6:E6"].CellStyle.VerticalAlignment = ExcelVAlign.VAlignTop;
+
+                //Thông tin khách hàng
+                worksheet.Range["A7"].Text = "  THÔNG TIN KHÁCH HÀNG";
+                worksheet.Range["A7"].CellStyle.Color = Color.FromArgb(0, 137, 71);
+                worksheet.Range["A7"].CellStyle.Font.Bold = true;
+                worksheet.Range["A7"].CellStyle.Font.Color = ExcelKnownColors.White;
+                worksheet.Range["A7"].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                worksheet.Range["A7"].CellStyle.VerticalAlignment = ExcelVAlign.VAlignCenter;
+
+                worksheet.Range["A8"].Text = "Phan Tấn Đạt";
+                worksheet.Range["A9"].Text = "Tường Lộc";
+                worksheet.Range["A10"].Text = "Tam Bình";
+                worksheet.Range["A11"].Text = "Vĩnh Long";
+                worksheet.Range["A12"].Text = "0123456789";
+
+                //Email
+                IHyperLink hyperlink = worksheet.HyperLinks.Add(worksheet.Range["A13"]);
+                hyperlink.Type = ExcelHyperLinkType.Url;
+                hyperlink.Address = "phantandat97@gmail.com";
+                hyperlink.ScreenTip = "Gửi mail";
+
+                worksheet.Range["A15:B15"].Merge();
+                worksheet.Range["A16:B16"].Merge();
+                worksheet.Range["A17:B17"].Merge();
+                worksheet.Range["A18:B18"].Merge();
+                worksheet.Range["A19:B19"].Merge();
+                worksheet.Range["A20:B20"].Merge();
+                worksheet.Range["A21:B21"].Merge();
+                worksheet.Range["A22:B22"].Merge();
+
+                //Thông tin chi tiết đơn hàng
+                worksheet.Range["A15"].Text = "  TÊN SẢN PHẨM";
+                worksheet.Range["C15"].Text = "SL";
+                worksheet.Range["D15"].Text = "ĐƠN GIÁ";
+                worksheet.Range["E15"].Text = "TỔNG";
+                worksheet.Range["A16"].Text = "Sản phẩm 1";
+                worksheet.Range["A17"].Text = "Sản phẩm 2";
+                worksheet.Range["A18"].Text = "Sản phẩm 3";
+                worksheet.Range["A19"].Text = "Sản phẩm 4";
+                worksheet.Range["A20"].Text = "Sản phẩm 5";
+                worksheet.Range["C16"].Number = 3;
+                worksheet.Range["C17"].Number = 2;
+                worksheet.Range["C18"].Number = 1;
+                worksheet.Range["C19"].Number = 4;
+                worksheet.Range["C20"].Number = 3;
+                worksheet.Range["D16"].Number = 21;
+                worksheet.Range["D17"].Number = 54;
+                worksheet.Range["D18"].Number = 10;
+                worksheet.Range["D19"].Number = 20;
+                worksheet.Range["D20"].Number = 30;
+                worksheet.Range["D23"].Text = "Tổng thanh toán";
+
+                //Format đơn vị tiền
+                worksheet.Range["D16:E22"].NumberFormat = "0.00vnd";
+                worksheet.Range["E23"].NumberFormat = "0.00vnd";
+
+                //M đổ tổng tiền từ csdl vào hay dùng excel để tính, cái này là dùng công thức excel để tính
+                application.EnableIncrementalFormula = true;
+                worksheet.Range["E16:E20"].Formula = "=C16*D16";
+                worksheet.Range["E23"].Formula = "=SUM(E16:E22)";
+
+                //Format borders
+                worksheet.Range["A16:E22"].CellStyle.Borders[ExcelBordersIndex.EdgeTop].LineStyle = ExcelLineStyle.Thin;
+                worksheet.Range["A16:E22"].CellStyle.Borders[ExcelBordersIndex.EdgeBottom].LineStyle = ExcelLineStyle.Thin;
+                worksheet.Range["A16:E22"].CellStyle.Borders[ExcelBordersIndex.EdgeTop].Color = ExcelKnownColors.Grey_25_percent;
+                worksheet.Range["A16:E22"].CellStyle.Borders[ExcelBordersIndex.EdgeBottom].Color = ExcelKnownColors.Grey_25_percent;
+                worksheet.Range["A23:E23"].CellStyle.Borders[ExcelBordersIndex.EdgeTop].LineStyle = ExcelLineStyle.Thin;
+                worksheet.Range["A23:E23"].CellStyle.Borders[ExcelBordersIndex.EdgeBottom].LineStyle = ExcelLineStyle.Thin;
+                worksheet.Range["A23:E23"].CellStyle.Borders[ExcelBordersIndex.EdgeTop].Color = ExcelKnownColors.Black;
+                worksheet.Range["A23:E23"].CellStyle.Borders[ExcelBordersIndex.EdgeBottom].Color = ExcelKnownColors.Black;
+
+                //Format font
+                worksheet.Range["A3:E23"].CellStyle.Font.FontName = "Arial";
+                worksheet.Range["A3:E23"].CellStyle.Font.Size = 10;
+                worksheet.Range["A15:E15"].CellStyle.Font.Color = ExcelKnownColors.White;
+                worksheet.Range["A15:E15"].CellStyle.Font.Bold = true;
+                worksheet.Range["D23:E23"].CellStyle.Font.Bold = true;
+
+                worksheet.Range["A15:E15"].CellStyle.Color = Color.FromArgb(0, 137, 71);
+
+                worksheet.Range["A15"].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignLeft;
+                worksheet.Range["C15:C22"].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignCenter;
+                worksheet.Range["D15:E15"].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignCenter;
+
+                worksheet.Range["A1"].ColumnWidth = 36;
+                worksheet.Range["B1"].ColumnWidth = 11;
+                worksheet.Range["C1"].ColumnWidth = 8;
+                worksheet.Range["D1:E1"].ColumnWidth = 18;
+                worksheet.Range["A1"].RowHeight = 47;
+                worksheet.Range["A2"].RowHeight = 15;
+                worksheet.Range["A3:A4"].RowHeight = 15;
+                worksheet.Range["A5"].RowHeight = 18;
+                worksheet.Range["A6"].RowHeight = 29;
+                worksheet.Range["A7"].RowHeight = 18;
+                worksheet.Range["A8"].RowHeight = 15;
+                worksheet.Range["A9:A14"].RowHeight = 15;
+                worksheet.Range["A15:A23"].RowHeight = 18;
+
+
+                //Saving the Excel to the MemoryStream 
+                MemoryStream stream = new MemoryStream();
+
+                workbook.SaveAs(stream);
+
+                //Set the position as '0'.
+                stream.Position = 0;
+
+                //Download the Excel file in the browser
+                FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/excel");
+
+                fileStreamResult.FileDownloadName = "HoaDon.xlsx";
+
+                return fileStreamResult;
             }
         }
     }
